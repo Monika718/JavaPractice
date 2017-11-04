c404c96c43e82775c5053f2536f8bec99e2c5b0f
Monika <zhong.xiabei@gmail.com>
2017-11-04 14:38:59 -0400

    BinaryTree 3 types of traversal
    Finished binary tree traversal

--- src/com/dataStructures/Main.java
+++ src/com/dataStructures/Main.java
@@ -3,28 +3,65 @@ package com.dataStructures;
 import static java.sql.Types.NULL;
 
 public class Main {
-    public static void createTree(int[] treeArray) {
-        BinaryTree tree = new BinaryTree(treeArray[0]);
-        for (int i = 1; i < treeArray.length; i++) {
-            BinaryTreeNode parent = new BinaryTreeNode(treeArray[i - 1]);
-            BinaryTreeNode child = new BinaryTreeNode(treeArray[i]);
-            if ( i % 2 == 0) {
-                tree.addTreeNode(parent, child, "right");
-            } else {
-                tree.addTreeNode(parent, child, "left");
-            }
+
+    public static void addTreeNode(BinaryTreeNode parent, BinaryTreeNode child, String direction) {
+        if (direction == "left") {
+            parent.setLeft(child);
+        }
+        if (direction == "right") {
+            parent.setRight(child);
         }
     }
 
-    public static void printTree(BinaryTree tree) {
-        int[] result = new int[10];
+    public static BinaryTreeNode createTree() {
+        BinaryTreeNode a = new BinaryTreeNode(1);
+        BinaryTreeNode b = new BinaryTreeNode(2,null, a);
+        BinaryTreeNode c = new BinaryTreeNode(3, b, null);
+        BinaryTreeNode d = new BinaryTreeNode(4);
+        BinaryTreeNode e = new BinaryTreeNode(5);
+        BinaryTreeNode f = new BinaryTreeNode(6, d, e);
+        BinaryTreeNode g = new BinaryTreeNode(7, c, f);
+        return g;
+    }
+
+    public static void visit(BinaryTreeNode node) {
+        System.out.println("Treenode Value: " + node.getValue());
+    }
+
+    public static void preOrder(BinaryTreeNode node) {
+        if (node != null) {
+            visit(node);
+            preOrder(node.getLeft());
+            preOrder(node.getRight());
+        }
+    }
+
+    public static void inOrder(BinaryTreeNode node) {
+        if (node != null) {
+            inOrder(node.getLeft());
+            visit(node);
+            inOrder(node.getRight());
+        }
+    }
+
+    public static void postOrder(BinaryTreeNode node) {
+        if (node != null) {
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            visit(node);
+        }
     }
 
     public static void main(String[] args) {
         // write your code here
-        int[] treeArray = {1, 2, 3, NULL, 4, 5};
-        BinaryTree tree = new BinaryTree(treeArray[0]);
-        createTree(treeArray);
-        System.out.println(tree);
+        System.out.println("Preorder:");
+        preOrder(createTree());
+
+        System.out.println("Inorder:");
+        inOrder(createTree());
+
+        System.out.println("Postorder:");
+        postOrder(createTree());
+
     }
 }
